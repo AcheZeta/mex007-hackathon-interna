fe075e3851069cde943e9ae216eed8444fc2cb1b
Hameyalli <ache.mariana@gmail.com>
2019-02-28 19:01:17 -0600

    Revert "Peliculas filtradas por genero en la data"

---
+++ test/data.spect.js
@@ -0,0 +1,13 @@
+// require('../src/data.js');
+
+// describe('Data', () => {
+//     it('Debería ser un objeto', () => {
+//         expect(typeof data).toBe('object');
+//     });
+// });
+
+// describe('Filter', () => {
+//     it('Debería ser una funcion', () => {
+//         expect(typeof window.data.filter).toBe('function');
+//     });
+// });
