f44975bed248a4e71a2ff9fcac2a125c215daac3
AcheZeta <ache.mariana@gmail.com>
2019-07-10 12:07:46 -0500

    SOLVE: Function filter by Name

--- src/data.js
+++ src/data.js
@@ -1,8 +1,17 @@
 window.movie = {
   filterByTitle: (allMovies, name2) => {
-    const filteringTitle = allMovies.filter(element => (element.Title.toLowerCase().match(name2.toLowerCase()) || (element.Year.toLowerCase().match(name2.toLowerCase())) || (element.Director.toLowerCase().match(name2.toLowerCase()))))
-    console.log(element, 'element');
-    return filteringTitle;
+    console.log(allMovies, name2);
+    listName = []
+    allMovies.filter(element => {
+      if (element.hasOwnProperty('Title')) {
+        element.Title.toLowerCase().match(name2.toLowerCase()) ? listName.push(element) : ''
+        console.log(listName)
+      }
+    })
+    return listName
+    // allMovies.filter(element => (element.Title.toLowerCase().match(name2.toLowerCase()) || (element.Year.toLowerCase().match(name2.toLowerCase())) || (element.Director.toLowerCase().match(name2.toLowerCase()))))
+    // console.log(element, 'element');
+    // return filteringTitle;
   },
 
   filterByGenre: (allMovies, genreId) => {
