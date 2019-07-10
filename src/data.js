26d14d4259951be18281b3bbef532e4288e2bf47
AcheZeta <ache.mariana@gmail.com>
2019-07-10 10:55:53 -0500

    RESOLVE: Filter by genre

--- src/data.js
+++ src/data.js
@@ -1,14 +1,20 @@
 window.movie = {
   filterByTitle: (allMovies, name2) => {
-
+    
     const filteringTitle = allMovies.filter(element => (element.Title.toLowerCase().match(name2.toLowerCase())||(element.Year.toLowerCase().match(name2.toLowerCase()))||(element.Director.toLowerCase().match(name2.toLowerCase()))))
     return filteringTitle;
   },
-
+  
   filterByGenre: (allMovies, genreId) => {
-    const filtering = allMovies.filter(element => element.Genre.includes(genreId));
-    return filtering;
+    let list = []
+    const filtering = allMovies.filter(element => {
+      if(element.hasOwnProperty('Genre')) {
+        element.Genre.includes(genreId) ? list.push(element) : ''
+      }
+    });
+    return list;
   },
-
+  
   //Esta llave cierra window
 }
+
